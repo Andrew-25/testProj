cddb5cf0051db2d733ea34880c90c01ab95b6339
Andrew-25 <andrewoozs@hotmail.com>
2023-12-20 15:49:56 +0000

    git commiting

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import TestButton from './components/TestButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TestButton />
       <StatusBar style="auto" />
     </View>
   );
